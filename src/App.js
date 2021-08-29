bfd5eb335bcc2dff1dc1d79d64ff7a89cc911456
nit567 <nitishkumarsingh410@gmail.com>
2021-08-30 01:14:31 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from "react";
-import {ChatEngine} from "react-chat-engine";
-import ChatFeed from "./components/ChatFeed";
+import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-const App =() =>{
-    return(
-        <ChatEngine
-          height="100vh"
-          projectID="c1717371-f854-4ff9-88f3-1ce81623e762"
-          userName="Nitish410"
-          userSecret="123123"
-          renderChatFeed={(chatAppsProps) => <ChatFeed {...chatAppsProps}/>}
-        />
-    );
-}
+const projectID = "c1717371-f854-4ff9-88f3-1ce81623e762";
+
+const App = () => {
+   
+  <LoginForm />
+
+  if (!localStorage.getItem('username')) return(<LoginForm /> );
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+//{localStorage.getItem('username')}
+//{localStorage.getItem('password')}
+//<LoginForm />
+
+// infinite scroll, logout, more customizations...
 
 export default App;
